34ffb3b2105bc104aebd7ace50bdd27ab4255b23
Andrea <andryx1994@gmail.com>
2020-06-10 16:38:08 +0200

    Versione 1.0
    Implementati dropdown menù e sistemato codice HTML, CSS E JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,51 @@
 $(document).ready(
   function() {
+    //azione passaggio mouse
+    $("#header-right li").mouseenter(
+      function() {
+        $(this).children().addClass("active");
+      }
+    )
+
+    //azione uscita mouse
+    $("#header-right li").mouseleave(
+      function() {
+        $(this).children().removeClass("active");
+      }
+    )
+
+    //azione click mouse
+    $("#header-right li").click(
+      function() {
+        $(this).children().toggleClass("active");
+      }
+    )
+
+    //azione passaggio mouse selettore lingua
+    $("#selettore-lingua").mouseenter(
+      function() {
+        scambioIcona($("#lingua"));
+      }
+    )
+
+    //azione uscita mouse selettore lingua
+    $("#selettore-lingua").mouseleave(
+      function() {
+        scambioIcona($("#lingua"));
+      }
+    )
   }
 );
+
+//FUNZIONE scambio icona "chevron"
+function scambioIcona(icona){
+  if (icona.hasClass("fa-chevron-up")){
+    icona.removeClass("fa-chevron-up");
+    icona.addClass("fa-chevron-down");
+  }
+
+  else {
+    icona.removeClass("fa-chevron-down");
+    icona.addClass("fa-chevron-up");
+  }
+}
